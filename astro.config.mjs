ca052d16e40af22c9f3967ed61af5a5aa40b68d2
hanagondou <burin72@yahoo.com>
2026-06-10 21:59:16 +0200

    Fix Worker name and KV namespace in Astro config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,12 @@
 import { defineConfig } from 'astro/config';
+import cloudflare from '@astrojs/cloudflare';
 
 export default defineConfig({
   output: 'static',
+  adapter: cloudflare({
+    workerName: 'championship-bracket',
+    kvNamespaces: [
+      { binding: 'SESSION', id: '1157f6e682a74df1a26184458b44bd8a' }
+    ]
+  })
 });
