ea0db40d15e6472cdc0421410b439d3af56404f0
hanagondou <burin72@yahoo.com>
2026-06-11 21:12:10 +0200

    Trivia & Squads II

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,6 @@ import { defineConfig } from 'astro/config';
 import cloudflare from '@astrojs/cloudflare';
 
 export default defineConfig({
-  output: 'server',
-  adapter: cloudflare({ mode: 'advanced' })
+  output: 'static',
+  adapter: cloudflare()
 });
