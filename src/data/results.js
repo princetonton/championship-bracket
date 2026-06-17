a22e3279681ac1bc4da6b64615a61904f9b1d8d8
hanagondou <burin72@yahoo.com>
2026-06-17 07:45:51 +0200

    update I

--- src/data/results.js
+++ src/data/results.js
@@ -1,8 +1,34 @@
 export const RESULTS = {
+  // Group A
   A_01: { homeScore: 2, awayScore: 0 },
   A_02: { homeScore: 2, awayScore: 1 },
+  // Group B
   B_01: { homeScore: 1, awayScore: 1 },
+  B_02: { homeScore: 1, awayScore: 1 },
+  // Group C
+  C_01: { homeScore: 1, awayScore: 1 },
+  C_02: { homeScore: 0, awayScore: 1 },
+  // Group D
   D_01: { homeScore: 4, awayScore: 1 },
+  D_02: { homeScore: 2, awayScore: 0 },
+  // Group E
+  E_01: { homeScore: 7, awayScore: 1 },
+  E_02: { homeScore: 1, awayScore: 0 },
+  // Group F
+  F_01: { homeScore: 2, awayScore: 2 },
+  F_02: { homeScore: 5, awayScore: 1 },
+  // Group G
+  G_01: { homeScore: 1, awayScore: 1 },
+  G_02: { homeScore: 2, awayScore: 2 },
+  // Group H
+  H_01: { homeScore: 0, awayScore: 0 },
+  H_02: { homeScore: 1, awayScore: 1 },
+  // Group I
+  I_01: { homeScore: 3, awayScore: 1 },
+  I_02: { homeScore: 1, awayScore: 4 },
+  // Group J
+  J_01: { homeScore: 3, awayScore: 0 },
+  J_02: { homeScore: 1, awayScore: 1 },
 };
 
 export function getResult(fixtureId) {
